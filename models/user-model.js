eaaccf6145c6f292bafe5ffa5c526f4c6f77ceb2
Sumit Kumar Shaw <sumitkumarshaw@SUMITs-MacBook-Air.local>
2025-01-22 11:13:41 +0530

    created add to cart feature, flash messages feature

--- models/user-model.js
+++ models/user-model.js
@@ -1,16 +1,22 @@
 const mongoose = require("mongoose");
 
-const joi=require("joi");
+const joi = require("joi");
 
 const userSchema = mongoose.Schema({
     fullName: String,
     email: String,
     password: String,
     contact: String,
-    cart: {
-        type: Array,
-        default: [],
-    },
+    cart: [{
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "product",
+        },
+        quantity: {
+            type: Number,
+            default: 1,
+        }
+    }],
     orders: {
         type: Array,
         default: [],
@@ -18,26 +24,29 @@ const userSchema = mongoose.Schema({
     picture: String,
 });
 
-const userJoiSchema=joi.object({
+const userJoiSchema = joi.object({
     fullName: joi.string().min(3).max(50).required(),
     email: joi.string().email().required(),
     password: joi.string().min(6).max(50).required(),
     contact: joi.string().min(10).max(15).optional(),
-    cart: joi.array().items(joi.string()).optional(),
+    cart: joi.array().items(joi.object({
+        product: joi.string().required(),
+        quantity: joi.number().default(1),
+    })).optional(), 
     orders: joi.array().items(joi.string()).optional(),
     picture: joi.string().uri().optional(),
 });
 
-const loginJoiSchema=joi.object({
+const loginJoiSchema = joi.object({
     email: joi.string().email().required(),
     password: joi.string().min(6).max(50).required(),
 });
 
-userSchema.statics.validateUser=function(data){
+userSchema.statics.validateUser = function (data) {
     return userJoiSchema.validate(data);
 }
 
-userSchema.statics.validateLogin=function(data){
+userSchema.statics.validateLogin = function (data) {
     return loginJoiSchema.validate(data);
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,73 @@
-const express=require("express");
+const express = require("express");
 
-const router=express.Router();
+const router = express.Router();
 
-const isLoggedIn=require("../middlewares/isLoggedIn");
+const isLoggedIn = require("../middlewares/isLoggedIn");
 
-const productModel=require("../models/product-model");
+const productModel = require("../models/product-model");
+const userModel = require("../models/user-model");
 
-router.get("/", (req, res)=>{
-    let error=req.flash("error");
+router.get("/", (req, res) => {
+    let error = req.flash("error");
 
-    res.render("index", {error, logged: false});
+    res.render("index", { error, logged: false });
 })
 
-router.get("/shop", isLoggedIn, async (req, res)=>{
-    let products=await productModel.find();
+router.get("/shop", isLoggedIn, async (req, res) => {
+    let products = await productModel.find();
 
-    res.render("shop", {products});
+    let success = req.flash("success");
+    let error = req.flash("error");
+
+    res.render("shop", { products, success, error });
 })
 
-router.get("/logout", (req, res)=>{
+router.get("/addtocart/:id", isLoggedIn, async (req, res) => {
+    try {
+        let product = await productModel.findOne({ _id: req.params.id });
+
+        if (!product) {
+            req.flash("error", "Product not found");
+
+            return res.redirect("/shop");
+        }
+
+        let user = await userModel.findOne({ email: req.user.email });
+
+        if (!user) {
+            req.flash("error", "User not found");
+
+            return res.redirect("/shop");   
+        }
+
+        let cartProduct=user.cart.find(item=>item.product.toString()===req.params.id);
+
+        if(cartProduct){
+            cartProduct.quantity++;
+        }
+        else{
+            user.cart.push({
+                product: req.params.id,
+                quantity: 1,
+            });
+        }
+
+        await user.save();
+
+        req.flash("success", "Product added to cart");
+
+        res.status(200).redirect("/shop");
+    } catch (err) {
+        req.flash("error", err.message);
+
+        res.status(500).redirect("/shop");
+    }
+})
+
+router.get("/logout", (req, res) => {
     res.cookie("token", "");
 
     res.status(200).redirect("/");
 });
 
-module.exports=router;
+module.exports = router;
